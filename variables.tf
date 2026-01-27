1deab8d99c31997e6c29056618875881c68080de
James Armes <jarmes@codeforamerica.org>
2026-01-27 12:38:20 -0500

    fix: Remove domain validation.

--- variables.tf
+++ variables.tf
@@ -1,11 +1,6 @@
 variable "domain" {
   type        = string
   description = "The domain to use for the SES email address."
-
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]\\.[a-zA-Z]{2,}$", var.domain))
-    error_message = "The domain must be a valid domain name."
-  }
 }
 
 variable "hosted_zone_id" {
